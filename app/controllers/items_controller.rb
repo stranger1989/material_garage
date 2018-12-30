693c313bc36a1d725528ed0ca17b165f0cb26cab
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-30 13:54:27 +0900

    Create_showFunction

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:show]
 
   def index
+    @items = Item.where("user_id = ?",current_user.id)
+  end
+
+  def show
   end
 
   def new
@@ -16,6 +21,10 @@ class ItemsController < ApplicationController
   private
   def create_params
     params.require(:item).permit(:name,:image_url,:storage,:memo).merge(user_id:current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
