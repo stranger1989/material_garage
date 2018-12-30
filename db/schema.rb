108a06f6ba983fcb2274ae2f51f3d9c35347c304
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-30 11:43:41 +0900

    AddUserIdToItemModel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_29_111138) do
+ActiveRecord::Schema.define(version: 2018_12_30_013717) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_12_29_111138) do
     t.text "QRcode_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
