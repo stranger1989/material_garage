8563fb9bf4bd88454c7ebef4a9a428f29a7770a1
kuroda-kentaro <kuroda_business@outlook.jp>
2019-01-01 13:02:05 +0900

    Add_itemDeleteEdit_Function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :destroy, :edit, :update]
 
   def index
     @items = Item.where("user_id = ?",current_user.id)
@@ -16,6 +16,19 @@ class ItemsController < ApplicationController
   def create
     Item.create(create_params)
     redirect_to action: :index
+  end
+
+  def destroy
+    @item.destroy if @item.user_id == current_user.id
+    redirect_to action: :index
+  end
+
+  def update
+    @item.update(create_params) if @item.user_id == current_user.id
+    redirect_to action: :index
+  end
+
+  def edit
   end
 
   private
